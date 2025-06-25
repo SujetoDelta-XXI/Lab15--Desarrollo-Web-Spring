153e18ff9f9c999637993a6954642bdaee958e99
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2025-06-25 15:20:21 -0500

    Primer commit

---
+++ src/main/java/com/tecsup/demo/Lab15DesarrolloWebAsparrinApplication.java
@@ -0,0 +1,13 @@
+package com.tecsup.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab15DesarrolloWebAsparrinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab15DesarrolloWebAsparrinApplication.class, args);
+    }
+
+}
